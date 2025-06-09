ff2a2decefcbed071da655f8d978968772f492f7
Patricklt2 <108370425+Patricklt2@users.noreply.github.com>
2025-06-09 20:43:11 -0300

    Update tests_main.c

--- src/tests/tests_main.c
+++ src/tests/tests_main.c
@@ -9,15 +9,15 @@ int main() {
 
 
 
-    test_cover_and_recover_k(2, 2, "assets/Albertssd.bmp", "encodings/test1.bmp");
-    test_cover_and_recover_k(3, 3, "assets/Albertssd.bmp", "encodings/test2.bmp");
-    test_cover_and_recover_k(4, 4, "assets/Albertssd.bmp", "encodings/test3.bmp");
-    test_cover_and_recover_k(5, 5, "assets/Albertssd.bmp", "encodings/test4.bmp");
-    test_cover_and_recover_k(6, 6, "assets/Albertssd.bmp", "encodings/test5.bmp");
-    test_cover_and_recover_k(7, 7, "assets/Albertssd.bmp", "encodings/test6.bmp");
-    test_cover_and_recover_k(8, 8, "assets/Albertssd.bmp", "encodings/test7.bmp");
-    test_cover_and_recover_k(9, 9, "assets/Albertssd.bmp", "encodings/test8.bmp");
-    test_cover_and_recover_k(10, 10, "assets/Albertssd.bmp", "encodings/test9.bmp");
+    test_cover_and_recover_k(2, 2, "assets/Alfredssd.bmp", "encodings/test1.bmp");
+    test_cover_and_recover_k(3, 3, "assets/Alfredssd.bmp", "encodings/test2.bmp");
+    test_cover_and_recover_k(4, 4, "assets/Alfredssd.bmp", "encodings/test3.bmp");
+    test_cover_and_recover_k(5, 5, "assets/Alfredssd.bmp", "encodings/test4.bmp");
+    test_cover_and_recover_k(6, 6, "assets/Alfredssd.bmp", "encodings/test5.bmp");
+    test_cover_and_recover_k(7, 7, "assets/Alfredssd.bmp", "encodings/test6.bmp");
+    test_cover_and_recover_k(8, 8, "assets/Alfredssd.bmp", "encodings/test7.bmp");
+    test_cover_and_recover_k(9, 9, "assets/Alfredssd.bmp", "encodings/test8.bmp");
+    test_cover_and_recover_k(10,10,"assets/Alfredssd.bmp", "encodings/test9.bmp");
 
 
     printf("All tests completed successfully!\n");
